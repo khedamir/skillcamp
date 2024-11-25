bd19592ada3fe5c1978b7bacfffd41ec6c8693c5
kheda <kheda.amirova@gmail.com>
2024-11-25 12:48:27 +0300

    home-page

--- src/pages/homePage/index.tsx
+++ src/pages/homePage/index.tsx
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom";
+import logo from "../../assets/logo.svg";
 
 const HomePage = () => {
   return (
-    <div>
+    <div className="home-page">
+      <img src={logo} alt="SkillCamp" />
       <h1>SkillCamp</h1>
       <Link to={"/login"}>Авторизоваться</Link>
     </div>
